d3a7a60e5a8389fe68d1f8f54b8dc8e74befdffb
shubham <124447665+s7917@users.noreply.github.com>
2023-04-23 11:04:50 +0530

    Create display day name by elseif ladder.cpp

---
+++ display day name by elseif ladder.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cout<<"enter the value of day"<<endl;
+    cin>>day;
+    if(day==1){
+        cout<<"sun"<<endl;
+    }
+    else if(day==2){
+        cout<<"mon"<<endl;
+    }
+     else if(day==3){
+        cout<<"tues"<<endl;
+    }
+     else if(day==4){
+        cout<<"wed"<<endl;
+    }
+     else if(day==5){
+        cout<<"thurs"<<endl;
+    }
+     else if(day==6){
+        cout<<"fri"<<endl;
+    }
+     else if(day==7){
+        cout<<"sat"<<endl;
+    }
+    else{
+        cout<<"invalid value"<<endl;
+    }
+    
+    
+    
+    
+    return 0;
+}
